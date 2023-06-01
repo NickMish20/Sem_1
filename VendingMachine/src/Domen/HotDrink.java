f0fe92636c7104647919b9cf614d130143946c6d
NickMish <nickmish@mail.ru>
2023-06-01 19:30:37 +0300

    Commit 1

---
+++ VendingMachine/src/Domen/HotDrink.java
@@ -0,0 +1,31 @@
+package Domen;
+
+public class HotDrink extends Product {
+    private int temperature;
+    
+            /**
+         * Create a product for VM
+         *
+         * @param productId       id product
+         * @param productName
+         * @param productCategory
+         * @param price
+         * @throws Exception
+         */
+    public HotDrink(int productId, String productName, String productCategory, double price, int temperature) throws Exception {
+        super(productId, productName, productCategory, price);
+        this.temperature = temperature;
+    }
+    @Override
+    public String toString()
+    {
+        return "HotDrink{" +
+        "name='" + super.getProductName() + '\'' +
+        "category='" + super.getProductCategory() + '\'' +
+        ", cost=" + super.getPrice() +
+        ", temperature=" + temperature +
+        '}';
+    }
+
+
+}
